6ad03a1d1995dfe8c197d8afa2dd047872403c3c
Jonas Persson <jonas.persson111@gmail.com>
2026-01-29 14:00:06 +0100

    delete option added,

--- CatDbExtensions.cs
+++ CatDbExtensions.cs
@@ -2,8 +2,9 @@ using CatImageApi.Data;
 
 public static class CatDbExtensions
 {
-    public static void SetUpDatabase(this CatDbContext db)
+    public static void SetUpDatabase(this CatDbContext db, bool deleteDb = false)
     {
+        if (deleteDb == true) db.Database.EnsureDeleted();
         db.Database.EnsureCreated();
     }
 }
